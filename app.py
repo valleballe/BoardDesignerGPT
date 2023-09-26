25886e95adef200abda24b60e7397066149bddff
valleballe <valdemar.muda@gmail.com>
2023-09-26 16:35:16 -0400

    Added serial monitor code

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.abspath(__file__), 'utils'))
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'utils/Serial_Monitor'))
 
 from utils.llm import GPT4
-from utils.Serial_Monitor.monitor import monitor
+from utils.serial_monitor import monitor
 
 from settings import port
 from settings import board_fqbn
