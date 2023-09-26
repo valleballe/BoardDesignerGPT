8933d675abe227513296325d7340ebf2d01c9555
valleballe <valdemar.muda@gmail.com>
2023-09-26 13:52:11 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-OPENAI_API_KEY = "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" # your API key from https://platform.openai.com/account/api-keys
+OPENAI_API_KEY = "YOUR_API_KEY" # your API key from https://platform.openai.com/account/api-keys
 
 port = "/dev/cu.usbserial-110"  # Replace with your Arduino Nano's port (e.g., COM3 for Windows)
 board_fqbn = "arduino:avr:nano:cpu=atmega328" # Fully Qualified Board Name (FQBN). Replace with whatever board you are using.
