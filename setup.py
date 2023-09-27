12c5c83bc83522b761098c98a4ee8af120756130
valleballe <valdemar.muda@gmail.com>
2023-09-26 22:25:57 -0400

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 import os
 
-# Download the serial monitor repo
-os.system("git clone https://github.com/PBahner/Serial-Monitor.git utils/Serial_Monitor")
-
-# Create __init__.py if it doesn't exist
-#init_file = "utils/Serial_Monitor/__init__.py"
-#if not os.path.exists(init_file):
-#    open(init_file, 'a').close()
-
 # Install packages
 os.system("pip install openai pyserial")
